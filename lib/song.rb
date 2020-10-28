c9a34b64271d1817f747cacef3d825070d838f86
jonathanx111 <jonathanx111@gmail.com>
2020-10-28 19:25:03 -0400

    final

--- lib/song.rb
+++ lib/song.rb
@@ -1,65 +1,75 @@
+
 require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
   def self.all
     @@all
   end
-
+  
   def save
     self.class.all << self
   end
-
+  
   def self.create
     song = self.new
     song.save
     song
   end
 
-  def self.new_by_name(string)
+  def self.new_by_name(name)
     song = self.new
-    song.name = string
+    song.name = name
     song
   end
 
-  def self.create_by_name(string)
-    song = self.new  
-    song.name = string
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
     song.save
     song
   end
 
-  def self.find_by_name(string)
-    @@all.find do |song|
-      string == song.name
+  def self.find_by_name(name)
+    @@all.find{|x| x.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
     end
   end
 
-  def self.find_or_create_by_name(string)
-    self.find_by_name(string) || self.create_by_name(string)
-  end
-
   def self.alphabetical
-    self.all.sort_by { |s| s.name}
+    @@all.sort_by{|x| x.name}
   end
-
-   def self.new_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].gsub(".mp3", "")
-
-    song = self.new
-    song.name = song_name
-    song.artist_name = artist_name
+  
+  def self.new_from_filename(name)
+    new = name.split("-")
+    new1 = new.map{|x| x.strip}
+    song = Song.new
+    song.artist_name = new1[0]
+    song.name = new1[1].gsub(".mp3", "")
     song
+   # binding.pry
   end
 
-  def self.create_from_filename(filename)
-    self.new_from_filename(filename).save
+  def self.create_from_filename(name)
+    new = name.split("-")
+    new1 = new.map{|x| x.strip}
+    song = Song.new
+    song.artist_name = new1[0]
+    song.name = new1[1].gsub(".mp3", "")
+    song.save
   end
 
   def self.destroy_all
-    @@all = []
+    self.all.clear
   end
+
 end
+#binding.pry
